29122a610bdb50e7e5b2ceaa79b76a01f6416eaf
蔡于飛 <54766809+zhmgczh@users.noreply.github.com>
2026-04-29 20:42:31 -0400

    Update.

---
+++ Java/Decirational/src/Parser.java
@@ -0,0 +1,14 @@
+enum OperandType {
+    INTEGER,
+    RATIONAL,
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DIVIDE,
+    MODULO,
+    LEFT_PARENTHESIS,
+    RIGHT_PARENTHESIS
+}
+
+public class Parser {
+}
