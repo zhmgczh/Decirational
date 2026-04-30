2fbeb5e00c28c0d597b3a7366060116c23a9b14e
蔡于飛 <54766809+zhmgczh@users.noreply.github.com>
2026-04-30 12:48:23 -0400

    Update.

--- Java/Decirational/src/Parser.java
+++ Java/Decirational/src/Parser.java
@@ -1,7 +1,5 @@
-import java.util.Enumeration;
-
 enum TokenType {
-    INTEGER('i'), RATIONAL('r'), PLUS('+'), MINUS('-'), MULTIPLICATION('*'), DIVISION('/'), MODULO('%'), LEFT_PARENTHESIS('('), RIGHT_PARENTHESIS(')');
+    INTEGER('i'), LARGE_INTEGER('l'), RATIONAL('r'), PLUS('+'), MINUS('-'), MULTIPLICATION('*'), DIVISION('/'), MODULO('%'), POWER('^'), LEFT_PARENTHESIS('('), RIGHT_PARENTHESIS(')');
     private final char type_code;
 
     TokenType(char type_code) {
@@ -14,10 +12,11 @@ enum TokenType {
 
     public static int get_priority(TokenType type) {
         return switch (type) {
-            case LEFT_PARENTHESIS, RIGHT_PARENTHESIS -> 3;
+            case LEFT_PARENTHESIS, RIGHT_PARENTHESIS -> 4;
+            case POWER -> 3;
             case MULTIPLICATION, DIVISION, MODULO -> 2;
             case PLUS, MINUS -> 1;
-            case INTEGER, RATIONAL -> 0;
+            case INTEGER, LARGE_INTEGER, RATIONAL -> 0;
         };
     }
 }
@@ -27,7 +26,7 @@ interface Token {
 }
 
 enum Operator implements Token {
-    PLUS('+'), MINUS('-'), MULTIPLICATION('*'), DIVISION('/'), MODULO('%');
+    PLUS('+'), MINUS('-'), MULTIPLICATION('*'), DIVISION('/'), MODULO('%'), POWER('^');
     private final char operator_code;
     Token left;
     Token right;
@@ -43,7 +42,7 @@ enum Operator implements Token {
 }
 
 enum Operand implements Token {
-    INTEGER('i'), RATIONAL('r');
+    INTEGER('i'), LARGE_INTEGER('l'), RATIONAL('r');
     private final char operand_code;
     Object value;
 
