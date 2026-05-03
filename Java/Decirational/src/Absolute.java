e46fc0f14d5aafa98033bbef6897c9a5c56469b4
蔡于飛 <54766809+zhmgczh@users.noreply.github.com>
2026-05-03 14:40:39 -0400

    Update.

--- Java/Decirational/src/Absolute.java
+++ Java/Decirational/src/Absolute.java
@@ -1,6 +1,7 @@
 public enum Absolute implements Token {
     ABSOLUTE('|');
     private final char type_code;
+    Token child;
 
     Absolute(char type_code) {
         this.type_code = type_code;
--- Java/Decirational/src/Floor.java
+++ Java/Decirational/src/Floor.java
@@ -1,6 +1,7 @@
 public enum Floor implements Token {
     LEFT_FLOOR('['), RIGHT_FLOOR(']');
     private final char type_code;
+    Token child;
 
     Floor(char type_code) {
         this.type_code = type_code;
